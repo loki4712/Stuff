925abf4ba80531d1bb271ef634ea6ee6b18e282b
Karsten Körner <kkoerner.ac@googlemail.com>
2024-02-05 17:53:42 +0100

    delete old

--- .github/workflows/hello.cs
+++
@@ -1,8 +0,0 @@
-// my first test class
-
-public class Test
-{
-    private bool myFirstBoolMember;
-
-    public global::System.Boolean MyFirstBoolMember { get => myFirstBoolMember; set => myFirstBoolMember = value; }
-}
